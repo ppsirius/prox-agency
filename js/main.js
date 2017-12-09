afd1e17b9b4f1e9e5d6cd334650ba2bf3539c347
Piotr Pupczyk <ppsirius@gmail.com>
2017-12-09 20:13:39 +0100

    slide navigation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,41 @@ import fullPage from './fullPage';
 window.addEventListener('load', init, false );
 
 function init() {
-  fullPage.initialize('.app', {
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-    menu: '#menu',
-  });
+  fullPage.initialize('.app');
+
+  addSliderListener();
 }
 
+let currentActiveSlide = 1,
+  slideElements = document.querySelectorAll('.section').length;
+
+function updateCurrentActiveSlide() {
+  let slide = document.querySelectorAll('.section');
+  slide.forEach((element, index) => {
+    if(element.classList.contains('active')) {
+      return currentActiveSlide = index;
+    }
+  })
+}
+
+function addSliderListener() {
+  let arrowUp = document.querySelector('.arrow-up'),
+    arrowDown = document.querySelector('.arrow-down');
+
+  arrowUp.addEventListener('click', prevSlide);
+  arrowDown.addEventListener('click', nextSlide);
+}
+
+function nextSlide() {
+  if(currentActiveSlide < slideElements) {
+    currentActiveSlide += 1;
+    fullPage.moveTo(currentActiveSlide)
+  }
+}
+
+function prevSlide() {
+  if(currentActiveSlide > 1 ) {
+    currentActiveSlide -= 1;
+    fullPage.moveTo(currentActiveSlide)
+  }
+}
