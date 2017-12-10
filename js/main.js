97bba5a58d5a403d83e69ff3aa6313a9b7d69860
Piotr Pupczyk <p.pupczyk@samsung.com>
2017-12-10 17:08:33 +0100

    switch to es6 class

--- js/main.js
+++ js/main.js
@@ -1,43 +1,6 @@
 
-import fullPage from './fullPage';
-window.addEventListener('load', init, false );
+import FullPage from './fullPage';
 
-function init() {
-  fullPage.initialize('.app');
-
-  addSliderListener();
-}
-
-let currentActiveSlide = 1,
-  slideElements = document.querySelectorAll('.section').length;
-
-function updateCurrentActiveSlide() {
-  let slide = document.querySelectorAll('.section');
-  slide.forEach((element, index) => {
-    if(element.classList.contains('active')) {
-      return currentActiveSlide = index;
-    }
-  })
-}
-
-function addSliderListener() {
-  let arrowUp = document.querySelector('.arrow-up'),
-    arrowDown = document.querySelector('.arrow-down');
-
-  arrowUp.addEventListener('click', prevSlide);
-  arrowDown.addEventListener('click', nextSlide);
-}
-
-function nextSlide() {
-  if(currentActiveSlide < slideElements) {
-    currentActiveSlide += 1;
-    fullPage.moveTo(currentActiveSlide)
-  }
-}
-
-function prevSlide() {
-  if(currentActiveSlide > 1 ) {
-    currentActiveSlide -= 1;
-    fullPage.moveTo(currentActiveSlide)
-  }
-}
+window.addEventListener('load', () => {
+  FullPage.init();
+});
