c4609250f2fb807a35a5438554657db68c94ee59
Piotr Pupczyk <p.pupczyk@samsung.com>
2018-01-10 17:25:00 +0100

    add debounce and circle radius

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,7 +5,10 @@ import fullpage from "fullpage.js";
 class Menu {
   constructor() {
     this.isOpen = false;
-    this.menuClipPath = document.querySelector("#menu_clip_path circle");
+    this.isAnimating = false;
+    this.menuClipPath = document.querySelector(".menu__circle__bg__svg circle");
+    this.menuElemnents = document.querySelectorAll(".menu__list__element");
+    this.circleRadius = this.getCircleRadius();
   }
 
   init() {
@@ -21,8 +24,13 @@ class Menu {
     this.marginRight = 16 + 28 / 2; // center to svg icon
     this.marginTop = 16 + 22 / 2;
     this.positionX = window.innerWidth - this.marginRight;
-    $("#menu_clip_path circle").attr("cx", this.positionX);
-    $("#menu_clip_path circle").attr("cy", this.marginTop);
+
+    $(".menu__circle__bg__svg").attr(
+      "viewBox",
+      `0 0 ${window.innerWidth} ${window.innerHeight}`
+    );
+    $(this.menuClipPath).attr("cx", this.positionX);
+    $(this.menuClipPath).attr("cy", this.marginTop);
   }
 
   scrollToSlide() {
@@ -37,13 +45,52 @@ class Menu {
     }
   }
 
+  getCircleRadius() {
+    let windowDimentions = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+
+    return Math.sqrt(
+      Math.pow(windowDimentions.width, 2) + Math.pow(windowDimentions.height, 2)
+    );
+  }
+
   tooggleMenu() {
-    if (this.isOpen) {
-      TweenMax.to(this.menuClipPath, 0.5, { attr: { r: 0 } });
-    } else {
-      TweenMax.to(this.menuClipPath, 0.5, { attr: { r: 2000 } });
+    this.getCircleRadius();
+    if (!this.isAnimating) {
+      this.isAnimating = true;
+      if (this.isOpen) {
+        TweenMax.to(this.menuElemnents, 0.2, {
+          opacity: 0,
+          y: -10,
+          onComplete: () =>
+            TweenMax.to(this.menuClipPath, 0.5, {
+              attr: { r: 0 },
+              onComplete: () => (this.isAnimating = false)
+            })
+        });
+      } else {
+        TweenMax.to(this.menuClipPath, 0.5, {
+          attr: { r: this.circleRadius },
+          onComplete: () => {
+            TweenMax.staggerTo(
+              this.menuElemnents,
+              0.5,
+              {
+                opacity: 1,
+                y: 0
+              },
+              0.05,
+              () => {
+                this.isAnimating = false;
+              }
+            );
+          }
+        });
+      }
+      this.isOpen = !this.isOpen;
     }
-    this.isOpen = !this.isOpen;
   }
 }
 
